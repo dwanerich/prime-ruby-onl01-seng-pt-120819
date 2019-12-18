530b886c54222de2d43cb2df2f63f8b352b6c4cb
dwane richards <dwanerich@gmail.com>
2019-12-18 02:27:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,8 +3,9 @@ def prime?(contender)
   array = [2,3,4,5,6,7,8,9]
   
   array.each do |num|
-    return false if contender % num == 0
-    
+    if contender % num == 0
+    return false
+    end
   end
   return true
 end
